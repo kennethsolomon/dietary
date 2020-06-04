667f330aa36f419ceca327d92d79eb93b993e1f8
Kenneth Solomon <synecders@gmail.com>
2020-06-04 12:43:42 +0800

    make the jquery and data tables offline

--- index.php
+++ index.php
@@ -328,8 +328,8 @@
 <!-- Light Bootstrap Dashboard DEMO methods, don't include it in your project! -->
 <script src="./assets/js/demo.js"></script>
 
-<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+<script src="./assets/js/jquery.dataTables.min.js"></script>
+<script src="./assets/js/dataTables.bootstrap4.min.js"></script>
 
 <script type="text/javascript">
     $(document).ready(function() {
